2fe2ef76e979d41b8f511fbb3cee909e1ed6dd8f
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-19 22:19:52 +0900

    Add date time format convert function and it test.

--- src/calmen2/core.clj
+++ src/calmen2/core.clj
@@ -6,7 +6,17 @@
 (def ^:const ^:dynamic *TAMA-LIBRARY* "https://www.library.metro.tokyo.jp/common/scripts/calendar/tama/data.json")
 (def ^:const ^:dynamic *CENTRAL-LIBRARY* "https://www.library.metro.tokyo.jp/common/scripts/calendar/central/data.json")
 
+(defn parse-date [date]
+  "parse yyyymmdd style string convert to yyyy/mm/dd"
+  (if (= (count date) 8)
+    (let [y (apply str (seq (take 4 date)))
+          m (apply str (seq (take 2 (drop 4 date))))
+          d (apply str (seq (take 2 (drop 6 date))))]
+      (str y "/" m "/" d))
+    nil))
+
 (defn get-json [address]
+  "get json from address (address is direct link to json)"
   (let [url (URL. address)]
     (with-open [r (io/reader (.openStream url))]
       (json/read r))))
--- test/calmen2/core_test.clj
+++ test/calmen2/core_test.clj
@@ -6,3 +6,8 @@
   (testing "get json resrouce from library web site."
     (is (map? (get-json *TAMA-LIBRARY*)))))
     
+(deftest test-parse-date
+  (testing "date format parser."
+    (is (= "2017/12/31" (parse-date "20171231")))
+    (is (= nil (parse-date "xxxxxxxxxx")))
+    (is (= "yyyy/mm/dd" (parse-date "yyyymmdd")))))
