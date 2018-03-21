848cc83dff27067e5631625c18f648e7cd1b9bfb
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-21 22:46:18 +0900

    Remove parse-date function.
    parse-date function is error of date time format by me.
    I removed it. and arrangement to iCal template format.
    
    And add building iCal format data functions.
    
    this commit is very large commit...

--- src/calmen2/core.clj
+++ src/calmen2/core.clj
@@ -2,13 +2,13 @@
   (:require [clojure.data.json :as json]
             [clojure.java.io :as io]
             [clj-time.format :as f]
-            [clj-time.core :as t])
+            [clj-time.core :as t]
+            [selmer.parser :as tmpl])
   (:import java.net.URL))
 
 (def ^:const ^:dynamic *TAMA-LIBRARY* "https://www.library.metro.tokyo.jp/common/scripts/calendar/tama/data.json")
 (def ^:const ^:dynamic *CENTRAL-LIBRARY* "https://www.library.metro.tokyo.jp/common/scripts/calendar/central/data.json")
-(def ^:const ^:dynamic *CALENDAR-TEMPLATE* "
-BEGIN:VCALENDAR
+(def ^:const ^:dynamic *CALENDAR-TEMPLATE* "BEGIN:VCALENDAR
 METHOD:PUBLISH
 VERSION:2.0
 PRODID:CALMEN-CLJ
@@ -28,23 +28,12 @@ DTSTART:19510909T000000
 TZNAME:JST
 TZOFFSETTO:+0900
 END:STANDARD
-END:VTIMEZONE
-{% for elm in ClosingList %}
-BEGIN:VEVENT
+END:VTIMEZONE{% for elm in ClosingList %}BEGIN:VEVENT
 TRANSP:OPAQUE
 SUMMARY:図書館休み
 DTSTART;VALUE=DATE:{{elm.StartDate}}
 DTEND;VALUE=DATE:{{elm.EndDate}}
-END:VEVENT
-{% endfor %}
-")
-
-(defn parse-date [date]
-  "parse yyyymmdd style string convert to yyyy/mm/dd"
-  (.format
-   (java.text.SimpleDateFormat. "yyyy/mm/dd")
-   (.parse
-    (java.text.SimpleDateFormat. "yyyymmdd") date)))
+END:VEVENT{% endfor %}")
 
 (defn filter-not-closing-day [closing-days]
   (into (sorted-map) (filter #(= "1" (:closed (fnext %))) closing-days)))
@@ -57,13 +46,22 @@ END:VEVENT
   ([g closing-days]
    (->> (keys closing-days)
         (map #(name %))
-        (map #(g %))
-        (map #(parse-date %))))
+        (map #(g %))))
   ([closing-days]
    (build-days identity closing-days)))
 
 (defn get-next-days [closing-days]
   (build-days get-next-day closing-days))
+
+(defn build-closing-list [start-days end-days]
+  {:ClosingList (map #(assoc {} :StartDate %1 :EndDate %2) start-days end-days)})
+
+(defn render-calendar [closing-list]
+  (tmpl/render *CALENDAR-TEMPLATE* closing-list))
+
+(defn write-to-file [file content]
+  (with-open [f (io/writer file)]
+    (.write f content)))
 
 (defn get-json [address]
   "get json from address (address is direct link to json)"
@@ -71,5 +69,14 @@ END:VEVENT
     (with-open [r (io/reader (.openStream url))]
       (json/read r :key-fn keyword))))
 
+(defn make-iCal [closing-days file]
+  (->> (build-closing-list (build-days closing-days) (get-next-days closing-days))
+      render-calendar
+      (write-to-file file)))
+
 (defn -main []
-  nil)
+  (let [tama (filter-not-closing-day (get-json *TAMA-LIBRARY*))
+        central (filter-not-closing-day (get-json *CENTRAL-LIBRARY*))]
+    (make-iCal tama "/tmp/library_closing_tama.ics")
+    (make-iCal central "/tmp/library_closing_central.ics")))
+    
